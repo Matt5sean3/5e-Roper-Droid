dec46a0f955d6d8abde6ccf64455dca7d8f3031f
Matthew Balch <matbalch@vt.edu>
2015-07-08 16:12:38 -0400

    Generalized loading of score components from JSON

--- src/main/java/com/github/harverst/roper/model/common/BasicScoreComponent.java
+++ src/main/java/com/github/harverst/roper/model/common/BasicScoreComponent.java
@@ -3,6 +3,12 @@ package com.github.harverst.roper.model.common;
 import com.github.harverst.roper.model.ScoreComponent;
 import java.lang.Comparable;
 
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.lang.Enum;
+import static java.lang.Enum.valueOf;
+
 /**
  * A ScoreComponent uses a component function to change the value of a score
  *
@@ -33,5 +39,39 @@ public abstract class BasicScoreComponent<P> implements ScoreComponent<P>
   {
     return phase;
   }
+  
+  // 
+  public static <P extends Enum<P> > ScoreComponent<P>
+    fromJson(Class<P> phaseType, JSONObject obj)
+  {
+    try
+    {
+      // Needs some sort of phaseType
+      int value = obj.getInt("value");
+      P phase = valueOf(phaseType, obj.getString("phase"));
+      switch(valueOf(BasicComponentType.class, obj.getString("type")))
+      {
+      case ADDITIVE:
+        return new AdditiveScoreComponent(phase, value);
+      case MULTIPLICATIVE:
+        return new MultiplicativeScoreComponent(phase, value);
+      case DIVISIVE:
+        return new DivisiveScoreComponent(phase, value);
+      default:
+        return null;
+      }
+    }
+    catch(JSONException e)
+    {
+      return null;
+    }
+  }
+}
+
+enum BasicComponentType
+{
+  ADDITIVE,
+  MULTIPLICATIVE,
+  DIVISIVE
 }
 
--- src/main/java/com/github/harverst/roper/model/fifthedition/FifthEditionCharacterComponent.java
+++ src/main/java/com/github/harverst/roper/model/fifthedition/FifthEditionCharacterComponent.java
@@ -3,6 +3,7 @@ package com.github.harverst.roper.model.fifthedition;
 import com.github.harverst.roper.model.Character;
 import com.github.harverst.roper.model.ScoreComponent;
 import com.github.harverst.roper.model.CharacterComponent;
+import com.github.harverst.roper.model.common.BasicScoreComponent;
 import com.github.harverst.roper.model.fifthedition.FifthEditionScoreType;
 import com.github.harverst.roper.model.fifthedition.FifthEditionPhase;
 import org.json.JSONObject;
@@ -20,12 +21,19 @@ import java.io.FileReader;
 public abstract class FifthEditionCharacterComponent
   implements CharacterComponent<FifthEditionScoreType, FifthEditionPhase>
 {
-  public static FifthEditionCharacterComponent loadFromFile(String filename)
+  public static CharacterComponent<FifthEditionScoreType, FifthEditionPhase>
+    fromJson(String source)
+    throws JSONException
   {
-    // 
-    JSONObject loaded = null;
-    // grab a new filename
-    return new FifthEditionCharacterJsonComponent(loaded);
+    JSONObject obj = new JSONObject(source);
+    return new FifthEditionCharacterJsonComponent(obj);
+  }
+  public static CharacterComponent<FifthEditionScoreType, FifthEditionPhase> 
+    fromJsonFile(String filename)
+    throws JSONException
+  {
+    // Open the json file and read all the contents
+    return fromJson(filename);
   }
 }
 
@@ -48,7 +56,8 @@ class FifthEditionCharacterJsonComponent extends FifthEditionCharacterComponent
         {
           try
           {
-            subList.add(FifthEditionScoreComponent.fromJson(
+            subList.add(BasicScoreComponent.fromJson(
+              FifthEditionPhase.class,
               subjson.getJSONObject(name)));
           }
           catch(JSONException e)
@@ -67,6 +76,7 @@ class FifthEditionCharacterJsonComponent extends FifthEditionCharacterComponent
   }
   public void composite(Character<FifthEditionScoreType, FifthEditionPhase> c)
   {
+    
   }
   public void seperate(Character<FifthEditionScoreType, FifthEditionPhase> c)
   {
