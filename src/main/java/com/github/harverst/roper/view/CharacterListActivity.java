4544f1e3f287137c85dc63658974ec6f5a01ea27
Matthew Balch <matbalch@vt.edu>
2015-07-21 00:36:00 -0400

    Started adding new activities, starting with character list screen

---
+++ src/main/java/com/github/harverst/roper/view/CharacterListActivity.java
@@ -0,0 +1,16 @@
+package com.github.harverst.roper.view;
+
+import com.github.harverst.roper.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CharacterListActivity extends Activity
+{
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.character_list_activity);
+  }
+}
+
