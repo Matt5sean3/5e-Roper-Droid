c7e6a5268858fc068a7f79e831db8a4fe4ef1b07
Matthew Balch <matbalch@vt.edu>
2015-07-24 00:10:43 -0400

    Added a credits page and started on character list and overview pages

--- src/main/java/com/github/harverst/roper/Roper5eDroid.java
+++ src/main/java/com/github/harverst/roper/Roper5eDroid.java
@@ -2,6 +2,11 @@ package com.github.harverst.roper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+import com.github.harverst.roper.view.CharacterListActivity;
+import com.github.harverst.roper.view.CreditsActivity;
 
 public class Roper5eDroid extends Activity
 {
@@ -11,5 +16,15 @@ public class Roper5eDroid extends Activity
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+  }
+  public void openCharacterListActivity(View v)
+  {
+    Intent characterListIntent = new Intent(this, CharacterListActivity.class);
+    startActivity(characterListIntent);
+  }
+  public void openCreditsActivity(View v)
+  {
+    Intent creditsIntent = new Intent(this, CreditsActivity.class);
+    startActivity(creditsIntent);
   }
 }
